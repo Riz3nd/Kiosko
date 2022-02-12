6597df44036fbb41b703cce37503f1a9c2cf1a39
Oscar Argaez <oscarargaes@wposs.com>
2022-02-12 08:44:10 -0500

    Init Commit

---
+++ app/src/main/java/com/curso/kiosko/IMainActivity.java
@@ -0,0 +1,22 @@
+package com.curso.kiosko;
+
+import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.curso.kiosko.databinding.ActivityMainBinding;
+
+public interface IMainActivity {
+    interface View{
+
+    }
+    interface Presenter{
+        void manejador(ActivityMainBinding bindings);
+        void changeTheme(AppCompatActivity activity);
+    }
+    interface Iterator{
+        void manejador(ActivityMainBinding bindings);
+        void changeTheme(AppCompatActivity activity);
+    }
+
+}
---
+++ app/src/main/java/com/curso/kiosko/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.curso.kiosko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_Kiosko2);
+        setContentView(R.layout.activity_main2);
+    }
+}
